c61e9c00266dde2972f7e48b4e7ca3d6e5fd015f
Mayur Zope <zopemayur666@gmail.com>
2026-09-11 15:34:47 +0530

    conversion in JS

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -5,7 +5,7 @@ let score = "mayur";
 
 let valueInNumber = Number(score);
 //console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+//console.log(valueInNumber); 
 
 // "33" => 33
 // "33abc" => NaN

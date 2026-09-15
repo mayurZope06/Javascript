3efeafb76ce84ac63ade29f6ce7e45db77a101fd
Mayur Zope <zopemayur666@gmail.com>
2026-09-15 16:12:43 +0530

    objects in JS

--- 11_objects.js
+++ 11_objects.js
@@ -26,7 +26,7 @@ const obj4 = { 5: "a", 6: "b" };
 // const obj3 = { obj1, obj2 }
 // const obj3 = Object.assign({}, obj1, obj2, obj4)
 
-const obj3 = { ...obj1, ...obj2 };
+const obj3 = { ...obj1, ...obj2 }; // spread the objects and then combine
 // console.log(obj3);
 
 const users = [
@@ -52,24 +52,3 @@ users[1].email;
 // console.log(Object.entries(tinderUser));
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
-
-const course = {
-  coursename: "js in hindi",
-  price: "999",
-  courseInstructor: "mayur",
-};
-
-// course.courseInstructor
-
-const { courseInstructor: instructor } = course;
-
-// console.log(courseInstructor);
-console.log(instructor);
-
-// {
-//     "name": "mayur",
-//     "coursename": "js in hindi",
-//     "price": "free"
-// }
-
-[{}, {}, {}];

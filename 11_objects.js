8840383dc41db0144edc0293b5962e81f4759376
Mayur Zope <zopemayur666@gmail.com>
2026-09-16 23:27:02 +0530

    Object de-structure and JSON API

--- 11_objects.js
+++ 11_objects.js
@@ -52,3 +52,29 @@ users[1].email;
 // console.log(Object.entries(tinderUser));
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "mayur"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
